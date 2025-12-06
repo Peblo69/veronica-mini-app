258fe83f75da2db66a107f861d1e65643b71823f
Peblo69 <peblo69@users.noreply.github.com>
2025-12-06 21:32:47 +0100

    Updates

---
+++ assets/arrow-left-xkWpsS0h-1765053156219.js
@@ -0,0 +1,1 @@
+import{c as o}from"./index-CQg0tZ1E-1765053156219.js";const e=[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]],r=o("arrow-left",e);export{r as A};
---
+++ assets/clock-BxD3HGbc-1765053156219.js
@@ -0,0 +1,1 @@
+import{c}from"./index-CQg0tZ1E-1765053156219.js";const o=[["path",{d:"M12 6v6l4 2",key:"mmk7yg"}],["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}]],r=c("clock",o);export{r as C};
