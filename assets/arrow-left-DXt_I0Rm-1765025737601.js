db8e3e19f005cdfb6d853b0d068a55ad6520adae
Peblo69 <peblo69@users.noreply.github.com>
2025-12-06 13:55:43 +0100

    Updates

---
+++ assets/arrow-left-DXt_I0Rm-1765025737601.js
@@ -0,0 +1,1 @@
+import{c as o}from"./index-DPlfigTV-1765025737601.js";const e=[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]],r=o("arrow-left",e);export{r as A};
--- assets/check-CFbT-Zq1-1765025695779.js
+++
@@ -1,1 +0,0 @@
-import{c}from"./index-BaGIvTIm-1765025695779.js";const e=[["path",{d:"M20 6 9 17l-5-5",key:"1gmf2c"}]],t=c("check",e);export{t as C};
