8712bf984e93aa238aa4d1d2a8c215116a9a6436
Peblo69 <peblo69@users.noreply.github.com>
2025-12-06 16:23:32 +0100

    Updates

---
+++ assets/check-Cs1VcDkE-1765034605872.js
@@ -0,0 +1,1 @@
+import{c}from"./index-BdYH7CKX-1765034605872.js";const e=[["path",{d:"M20 6 9 17l-5-5",key:"1gmf2c"}]],t=c("check",e);export{t as C};
---
+++ assets/ellipsis-Dzijbwgj-1765034605872.js
@@ -0,0 +1,1 @@
+import{c}from"./index-BdYH7CKX-1765034605872.js";const e=[["circle",{cx:"12",cy:"12",r:"1",key:"41hilf"}],["circle",{cx:"19",cy:"12",r:"1",key:"1wjl8i"}],["circle",{cx:"5",cy:"12",r:"1",key:"1pcz8c"}]],r=c("ellipsis",e);export{r as E};
