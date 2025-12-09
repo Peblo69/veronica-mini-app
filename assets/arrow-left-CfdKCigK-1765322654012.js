47679120e2f12c37cae9193f99f21b85512dc00d
Peblo69 <peblo69@users.noreply.github.com>
2025-12-10 00:24:21 +0100

    Updates

---
+++ assets/arrow-left-CfdKCigK-1765322654012.js
@@ -0,0 +1,1 @@
+import{c as o}from"./index-CDW6Jhyp-1765322654012.js";const e=[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]],r=o("arrow-left",e);export{r as A};
--- assets/chevron-down-C5O_Sk9U-1765321910423.js
+++
@@ -1,1 +0,0 @@
-import{c as o}from"./index-FORqJf9f-1765321910423.js";const n=[["path",{d:"m6 9 6 6 6-6",key:"qrunsl"}]],e=o("chevron-down",n);export{e as C};
