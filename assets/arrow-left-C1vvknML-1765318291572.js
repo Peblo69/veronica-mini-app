5042ca628ada524b1ca687424f5532bd32eead41
Peblo69 <peblo69@users.noreply.github.com>
2025-12-09 23:11:42 +0100

    Updates

---
+++ assets/arrow-left-C1vvknML-1765318291572.js
@@ -0,0 +1,1 @@
+import{c as o}from"./index-DyzUX6xO-1765318291572.js";const e=[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]],r=o("arrow-left",e);export{r as A};
--- assets/check-2WOZqQ2C-1765318095756.js
+++
@@ -1,1 +0,0 @@
-import{c}from"./index-CpmdxiMH-1765318095756.js";const e=[["path",{d:"M20 6 9 17l-5-5",key:"1gmf2c"}]],t=c("check",e);export{t as C};
