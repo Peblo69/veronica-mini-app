ddb6c23eaabaac097f621cc89adf60ef9172c4f8
Peblo69 <peblo69@users.noreply.github.com>
2025-12-09 22:15:31 +0100

    Updates

--- assets/clock-CcIZU813-1765313534941.js
+++
@@ -1,1 +0,0 @@
-import{c}from"./index-BNIw4U_K-1765313534941.js";const o=[["path",{d:"M12 6v6l4 2",key:"mmk7yg"}],["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}]],r=c("clock",o);export{r as C};
--- assets/loader-circle-CMyo9YM1-1765313534941.js
+++
@@ -1,1 +0,0 @@
-import{c}from"./index-BNIw4U_K-1765313534941.js";const e=[["path",{d:"M21 12a9 9 0 1 1-6.219-8.56",key:"13zald"}]],a=c("loader-circle",e);export{a as L};
