5f2a9c0b8a435e7123909a923df4c5dd95c70918
Peblo69 <peblo69@users.noreply.github.com>
2025-12-11 17:11:32 +0100

    Updates

---
+++ assets/arrow-left-D5JDdUz1-1765469481657.js
@@ -0,0 +1,1 @@
+import{c as o}from"./index-CFse4ww0-1765469481657.js";const e=[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]],r=o("arrow-left",e);export{r as A};
---
+++ assets/chevron-right-DdW_AnZN-1765469481657.js
@@ -0,0 +1,1 @@
+import{c as o}from"./index-CFse4ww0-1765469481657.js";const t=[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]],e=o("chevron-right",t);export{e as C};
