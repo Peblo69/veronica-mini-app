9c86734c116857e19dffbcf9459a816e7dbb1276
Peblo69 <peblo69@users.noreply.github.com>
2025-12-11 23:29:59 +0100

    Updates

--- assets/chevron-right-ByQR2dUV-1765491389144.js
+++
@@ -1,1 +0,0 @@
-import{c as o}from"./index-BNOM0_-M-1765491389144.js";const t=[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]],e=o("chevron-right",t);export{e as C};
--- assets/loader-circle-D1CGZoYs-1765491389144.js
+++
@@ -1,1 +0,0 @@
-import{c}from"./index-BNOM0_-M-1765491389144.js";const e=[["path",{d:"M21 12a9 9 0 1 1-6.219-8.56",key:"13zald"}]],a=c("loader-circle",e);export{a as L};
