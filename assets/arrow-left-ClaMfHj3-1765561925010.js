5065c1dbeee45ddf9a7007957e1330a502a2cfba
Peblo69 <peblo69@users.noreply.github.com>
2025-12-12 18:52:17 +0100

    Updates

---
+++ assets/arrow-left-ClaMfHj3-1765561925010.js
@@ -0,0 +1,1 @@
+import{c as o}from"./index-DKmagv3U-1765561925010.js";const e=[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]],r=o("arrow-left",e);export{r as A};
---
+++ assets/chevron-left-CKP7G_dH-1765561925010.js
@@ -0,0 +1,1 @@
+import{c as e}from"./index-DKmagv3U-1765561925010.js";const o=[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]],t=e("chevron-left",o);export{t as C};
