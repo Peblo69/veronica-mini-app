ac4c4ec2f5fad98a0e9b8e59acd72872cd2c0720
Peblo69 <peblo69@users.noreply.github.com>
2025-12-12 22:02:37 +0100

    Updates

--- assets/chevron-left-DusbBxYh-1765570304309.js
+++
@@ -1,1 +0,0 @@
-import{c as e}from"./index-BJiOEdd2-1765570304309.js";const o=[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]],t=e("chevron-left",o);export{t as C};
--- assets/clock-CeLi8Ibj-1765570304309.js
+++
@@ -1,1 +0,0 @@
-import{c}from"./index-BJiOEdd2-1765570304309.js";const o=[["path",{d:"M12 6v6l4 2",key:"mmk7yg"}],["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}]],r=c("clock",o);export{r as C};
