707d3a191884ed5147360f9745ba1226826b53e2
Peblo69 <peblo69@users.noreply.github.com>
2025-12-12 11:14:22 +0100

    Updates

--- assets/check-CDJESON_-1765512753796.js
+++
@@ -1,1 +0,0 @@
-import{c}from"./index-yrhK9tR5-1765512753796.js";const e=[["path",{d:"M20 6 9 17l-5-5",key:"1gmf2c"}]],t=c("check",e);export{t as C};
---
+++ assets/chevron-left-Cez26K9h-1765534449122.js
@@ -0,0 +1,1 @@
+import{c as e}from"./index-Ci5pCk-g-1765534449122.js";const o=[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]],t=e("chevron-left",o);export{t as C};
