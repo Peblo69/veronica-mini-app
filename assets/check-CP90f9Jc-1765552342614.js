7941c4d9b5a2d8058e25d1b821d0e2e3a2bc435a
Peblo69 <peblo69@users.noreply.github.com>
2025-12-12 16:12:33 +0100

    Updates

---
+++ assets/check-CP90f9Jc-1765552342614.js
@@ -0,0 +1,1 @@
+import{c}from"./index-CIKPRU-F-1765552342614.js";const e=[["path",{d:"M20 6 9 17l-5-5",key:"1gmf2c"}]],t=c("check",e);export{t as C};
---
+++ assets/chevron-right-CCL_j1Jg-1765552342614.js
@@ -0,0 +1,1 @@
+import{c as o}from"./index-CIKPRU-F-1765552342614.js";const t=[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]],e=o("chevron-right",t);export{e as C};
