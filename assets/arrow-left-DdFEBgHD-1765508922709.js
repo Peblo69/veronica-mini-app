b54962deaafda5ec0b2fe72f977a88e80ebf4e72
Peblo69 <peblo69@users.noreply.github.com>
2025-12-12 04:08:56 +0100

    Updates

---
+++ assets/arrow-left-DdFEBgHD-1765508922709.js
@@ -0,0 +1,1 @@
+import{c as o}from"./index-CAAdj9OU-1765508922709.js";const e=[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]],r=o("arrow-left",e);export{r as A};
---
+++ assets/chevron-right-4jFQEL0W-1765508922709.js
@@ -0,0 +1,1 @@
+import{c as o}from"./index-CAAdj9OU-1765508922709.js";const t=[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]],e=o("chevron-right",t);export{e as C};
