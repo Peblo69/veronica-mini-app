bf036f0a5a2edb1fd6c8c9ba4ca8c5bbf05b1c07
Peblo69 <peblo69@users.noreply.github.com>
2025-12-14 00:16:10 +0100

    Updates

--- assets/mail-XAB4jF6V-1765666997201.js
+++
@@ -1,1 +0,0 @@
-import{c as e}from"./index-BHS0ki3y-1765666997201.js";const o=[["path",{d:"m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7",key:"132q7q"}],["rect",{x:"2",y:"4",width:"20",height:"16",rx:"2",key:"izxlao"}]],a=e("mail",o);export{a as M};
