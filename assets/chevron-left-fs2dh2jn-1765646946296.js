6c0f72f2542828dfe2901adfe9db971d35c2e619
Peblo69 <peblo69@users.noreply.github.com>
2025-12-13 18:29:18 +0100

    Updates

---
+++ assets/chevron-left-fs2dh2jn-1765646946296.js
@@ -0,0 +1,1 @@
+import{c as e}from"./index-DLX4cGQ9-1765646946296.js";const o=[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]],t=e("chevron-left",o);export{t as C};
---
+++ assets/x-DTlPL5dl-1765646946296.js
@@ -0,0 +1,1 @@
+import{c as o}from"./index-DLX4cGQ9-1765646946296.js";const c=[["path",{d:"M18 6 6 18",key:"1bl5f8"}],["path",{d:"m6 6 12 12",key:"d8bk6v"}]],t=o("x",c);export{t as X};
