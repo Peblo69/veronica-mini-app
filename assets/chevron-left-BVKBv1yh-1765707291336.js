1da4bc34524d8408f80168c1b06b2109eb5c4c43
Peblo69 <peblo69@users.noreply.github.com>
2025-12-14 11:14:57 +0100

    Updates

---
+++ assets/chevron-left-BVKBv1yh-1765707291336.js
@@ -0,0 +1,1 @@
+import{c as e}from"./index-OA272f9B-1765707291336.js";const o=[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]],t=e("chevron-left",o);export{t as C};
---
+++ assets/x-CivgF8Lc-1765707291336.js
@@ -0,0 +1,1 @@
+import{c as o}from"./index-OA272f9B-1765707291336.js";const c=[["path",{d:"M18 6 6 18",key:"1bl5f8"}],["path",{d:"m6 6 12 12",key:"d8bk6v"}]],t=o("x",c);export{t as X};
