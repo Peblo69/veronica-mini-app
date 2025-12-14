e0e84e4ff998ae2dc27c9418dbddd1ceaad82b8d
Peblo69 <peblo69@users.noreply.github.com>
2025-12-14 21:09:26 +0100

    Updates

---
+++ assets/check-4FD723vP-1765742955473.js
@@ -0,0 +1,1 @@
+import{c}from"./index-CtVUZLpR-1765742955473.js";const e=[["path",{d:"M20 6 9 17l-5-5",key:"1gmf2c"}]],t=c("check",e);export{t as C};
---
+++ assets/chevron-left-BWNLdofL-1765742955473.js
@@ -0,0 +1,1 @@
+import{c as e}from"./index-CtVUZLpR-1765742955473.js";const o=[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]],t=e("chevron-left",o);export{t as C};
